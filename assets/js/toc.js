de66a2e067484d33b795ed7c5fad898a8873cd58
Eric M. Fink <emfink@gmail.com>
2023-12-11 10:43:26 -0500

    update anchor.js toc

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,8 +1,8 @@
 anchors.options = {
-    placement: 'right',
+    placement: 'left',
     visible: 'hover',
   };
-anchors.add('h1, h2, h3, h4');
+anchors.add('h2, h3, h4');
 generateTableOfContents(anchors.elements);
 
 // External code for generating a simple dynamic Table of Contents
